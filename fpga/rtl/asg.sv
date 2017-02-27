f51037f47fbed7acb727efe59c5e44b7029fcefa
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-02-27 17:25:20 +0100

    FPGA: ASF: attempt to fix RAM inference

--- fpga/rtl/asg.sv
+++ fpga/rtl/asg.sv
@@ -155,17 +155,22 @@ logic               sts_rdy;  // ready
 //  DAC buffer RAM
 ////////////////////////////////////////////////////////////////////////////////
 
+logic bus_ena;
+assign bus_ena = bus.wen | bus.ren;
+
 // CPU read/write access
 always @(posedge bus.clk)
 for (int unsigned i=0; i<2; i++) begin
-  if (bus.wen)  buf_mem [{bus.addr>>2,i[0]}] <= bus.wdata [16*i+:16];
-  if (bus.ren)  bus.rdata [16*i+:16] <= buf_mem [{bus.addr>>2,i[0]}];
+  if (bus_ena) begin
+                  bus.rdata [16*i+:16] <= buf_mem [{bus.addr>>2,i[0]}];
+    if (bus.wen)  buf_mem [{bus.addr>>2,i[0]}] <= bus.wdata [16*i+:16];
+  end
 end
 
 // CPU control signals
 always_ff @(posedge bus.clk)
 if (~bus.rstn)  bus.ack <= 1'b0;
-else            bus.ack <= bus.ren | bus.wen;
+else            bus.ack <= bus_ena;
 
 assign bus.err = 1'b0;
 
