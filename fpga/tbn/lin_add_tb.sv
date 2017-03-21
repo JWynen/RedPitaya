7d4dc4d28ee77c1949873d53733f1cc68001e5e6
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-03-21 17:47:29 +0100

    FPGA: fixing signal saturation issue

--- fpga/tbn/lin_add_tb.sv
+++ fpga/tbn/lin_add_tb.sv
@@ -1,40 +1,37 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Module: Linear transformation (gain, offset and saturation)
+// Module: Linear adder
 // Author: Iztok Jeras
 // (c) Red Pitaya  (redpitaya.com)
 ////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns / 1ps
 
-module linear_tb #(
+module lin_add_tb #(
   // clock time periods
   realtime  TP = 4.0ns,  // 250MHz
   // stream parameters
   int unsigned DN = 1,
   type DTI = logic signed [8-1:0], // data type for input
   type DTO = logic signed [8-1:0], // data type for output
-  int unsigned DWM = 16,           // data width for multiplier (gain)
-  int unsigned DWS = $bits(DTO)    // data width for summation (offset)
+  type DTS = DTI                   // data type for configuration
 );
 
 typedef DTI DTI_A [];
 typedef DTO DTO_A [];
 
 // system signals
-logic                  clk ;  // clock
-logic                  rstn;  // reset - active low
+logic clk ;  // clock
+logic rstn;  // reset - active low
 
 // configuration
-logic signed [DWM-1:0] cfg_mul;
-logic signed [DWS-1:0] cfg_sum;
+DTS cfg_sum;
 
 // stream input/output
 axi4_stream_if #(.DT (DTI)) sti (.ACLK (clk), .ARESETn (rstn));
 axi4_stream_if #(.DT (DTO)) sto (.ACLK (clk), .ARESETn (rstn));
 
 // calibration
-real gain   = 1.0;
-real offset = 0.1;
+real offset = -0.95;
 
 int unsigned error = 0;
 
@@ -48,7 +45,6 @@ always #(TP/2) clk = ~clk;
 // clocking 
 default clocking cb @ (posedge clk);
   input  rstn;
-  input  cfg_mul;
   input  cfg_sum;
 endclocking: cb
 
@@ -63,8 +59,7 @@ initial begin
   axi4_stream_pkg::axi4_stream_class #(.DT (DTO)) clo;
 
   // for now initialize configuration to an idle value
-  cfg_sum = (offset * 2**(DWS-1));
-  cfg_mul = (gain   * 2**(DWM-2));
+  cfg_sum = (offset * 2**($bits(DTS)-1));
 
   // initialization
   rstn = 1'b0;
@@ -95,7 +90,7 @@ initial begin
   $finish();
 end
 
-// calculate linear transformation
+// calculate lin_add transformation
 // TODO: implement actual calculation
 function automatic DTO_A calc (ref DTI_A dat);
   calc = new [dat.size()] (dat);
@@ -107,17 +102,15 @@ endfunction: calc
 
 axi4_stream_src #(.DT (DTI)) str_src (.str (sti));
 
-linear #(
+lin_add #(
   .DTI (DTI),
   .DTO (DTO),
-  .DWM (DWM),
-  .DWS (DWS)
-) linear (
+  .DTS (DTS)
+) lin_add (
   // stream input/output
   .sti      (sti    ),
   .sto      (sto    ),
   // configuration
-  .cfg_mul  (cfg_mul),
   .cfg_sum  (cfg_sum)
 );
 
@@ -128,8 +121,8 @@ axi4_stream_drn #(.DT (DTO)) str_drn (.str (sto));
 ////////////////////////////////////////////////////////////////////////////////
 
 initial begin
-  $dumpfile("linear_tb.vcd");
-  $dumpvars(0, linear_tb);
+  $dumpfile("lin_add_tb.vcd");
+  $dumpvars(0, lin_add_tb);
 end
 
-endmodule: linear_tb
+endmodule: lin_add_tb
